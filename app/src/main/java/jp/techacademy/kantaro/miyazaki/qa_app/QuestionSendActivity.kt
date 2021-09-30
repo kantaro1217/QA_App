7059bf8b281b75d798a3d3b08703ccb92ef48369
kantaro1217 <kanade9615@gmail.com>
2021-09-30 14:30:11 +0900

    error

---
+++ app/src/main/java/jp/techacademy/kantaro/miyazaki/qa_app/QuestionSendActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.kantaro.miyazaki.qa_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionSendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_send)
+    }
+}
